1b774d7ae9dcfb00c5830e9ad36e8d1f8f755906
Arthur Luis Dantas de Siqueira <arthur.dantas@unimedrecife.com.br>
2021-03-17 11:50:22 -0300

    configuração básica para começar o desenvolvimento

--- football_jb/settings.py
+++ football_jb/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'football_jb.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+
+        'ENGINE': os.getenv('ENGINE'),
+
+        'NAME': os.getenv('NAME'),
+
+        'USER': os.getenv('USER'),
+
+        'PASSWORD': os.getenv('PASSWORD'),
+
+        'HOST': os.getenv('HOST'),
+
+        'PORT': os.getenv('PORT'),
     }
+
 }
 
 
